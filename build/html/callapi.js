cf6b3762f8569a9ce169e6ef607c4ac5a14a9d99
Nestor Bonilla <nmora@delbridge.solutions>
2023-09-04 11:44:31 -0600

    unauthorized message WIP

--- build/html/callapi.js
+++ build/html/callapi.js
@@ -1,17 +1,26 @@
- function callRobexia(){
-		const API_KEY = "7g9bunOiTP0nd4lrZPqdt9QEQLVTLi91qMalb6kdvhfarrmkbGIyy3ox0MHYjcYv";  // Reemplaza con tu API key
+const axios = require('axios');
 
-		fetch('https://eu-central-1.aws.data.mongodb-api.com/app/robexiaapi-vjexe/endpoint/candlelast', {
-			  method: 'GET', 
-			  headers: {
-					'Content-Type': 'application/json',
-					'access_token': `Bearer ${API_KEY}`,
-                    
-				}
-                //,mode: 'cors',  // Asegúrate de que el modo sea 'cors'
-			})
-			.then(response => response.json())
-			.then(data => console.log(data))
-			.catch(error => console.error('Error:', error));
+function callRobexia() {
+  const API_KEY = "7g9bunOiTP0nd4lrZPqdt9QEQLVTLi91qMalb6kdvhfarrmkbGIyy3ox0MHYjcYv"; // Replace with your API key
 
+  // Define the request headers
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${API_KEY}`,
+  };
+
+  // Make a GET request to the API endpoint
+  axios.get('https://eu-central-1.aws.data.mongodb-api.com/app/robexiaapi-vjexe/endpoint/candlelast', { headers })
+    .then((response) => {
+      // Handle the successful response here
+      const data = response.data;
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle errors here
+      console.error('Error:', error);
+    });
 }
+
+// Call the function to make the API request
+callRobexia();
---
+++ build/node_approach/app.js
@@ -0,0 +1,37 @@
+const https = require('https');
+
+const API_KEY = "7g9bunOiTP0nd4lrZPqdt9QEQLVTLi91qMalb6kdvhfarrmkbGIyy3ox0MHYjcYv"; // Replace with your API key
+
+// Define the request headers
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${API_KEY}`,
+};
+
+const options = {
+  hostname: 'eu-central-1.aws.data.mongodb-api.com',
+  path: '/app/robexiaapi-vjexe/endpoint/candlelast',
+  method: 'GET',
+  headers: headers,
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  let data = '';
+
+  res.on('data', chunk => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log(data);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+// Send the request
+req.end();
